c8d119b50dc956a34faaed55a117884425f17edf
kevin <minghui.liu@trincoll.edu>
2016-03-16 21:16:36 -0400

    Add Product model and Product API Add underscore as dependency

--- models.js
+++ models.js
@@ -1,15 +1,23 @@
 var mongoose = require('mongoose');
+var _ = require('underscore');
 
 module.exports = function(wagner) {
   mongoose.connect('mongodb://localhost:27017/example');
 
   var Category = mongoose.model('Category', require('./category'), 'categories');
+  var Product = mongoose.model('Product', require('./product'), 'products');
 
-  wagner.factory('Category', function() {
-    return Category;
+  var models = {
+    Category: Category,
+    Product: Product
+  }
+
+  // For each model, register a factory
+  _.each(models, function(value, key) {
+    wagner.factory(key, function() {
+      return value;
+    });
   });
 
-  return {
-    Category: Category
-  };
+  return models;
 };
