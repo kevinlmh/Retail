50796541a7147b0f69eab6658fc9439caa2e767a
kevin <minghui.liu@trincoll.edu>
2016-03-18 16:25:31 -0400

    Add user and cart API

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var body-parser = require('body-parser');
 var status = require('http-status');
 
 module.exports = function(wagner) {
   var api = express.Router();
+  api.use(body-parser.json());
 
   api.get('/category/id/:id', wagner.invoke(function(Category) {
     return function(req, res) {
@@ -47,6 +49,30 @@ module.exports = function(wagner) {
       Product.find({ 'category.ancestors': req.params.id }).sort(sort).exec(handleMany.bind(null, 'products', res));
     };
   }));
+
+  api.put('/me/cart', wagner.invoke(function(User) {
+    return function(req, res) {
+      try {
+        var cart = req.body.data.cart;
+      } catch (e) {
+        return res.status(status.BAD_REQUEST).json({ error: 'No cart specified' });
+      }
+      req.user.data.cart = cart;
+      req.user.save(function(error, user) {
+        if (error) {
+          return res.status(status.INTERNAL_SERVER_ERROR).json({ error: error.toString() });
+        }
+        return res.json({ user: user });
+      });
+    };
+  }));
+
+  api.get('/me', function(req, res) {
+    if (!req.user) {
+      return res.status(status.UNAUTHORIZED).json({ error: 'Not logged in' });
+    }
+    req.user.populate({ path: 'data.cart.product', model: 'Product' }, handleOne.bind(null, 'user', res));
+  });
 
   return api;
 };
